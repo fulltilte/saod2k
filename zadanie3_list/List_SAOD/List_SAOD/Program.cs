f7c87f3768e70e14c18a42334a1c932fa8e6c1c4
Ivan Shvedov <d4zzy@bk.ru>
2022-10-17 23:44:41 +0300

    update

--- zadanie3_list/List_SAOD/List_SAOD/Program.cs
+++ zadanie3_list/List_SAOD/List_SAOD/Program.cs
@@ -1,4 +1,9 @@
 ﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 using System.IO;
 using System.Runtime;
 using System.Runtime.Versioning;
@@ -7,15 +12,17 @@ using System.Diagnostics.Contracts;
 using System.Collections.ObjectModel;
 using System.Security.Permissions;
 
-saod s = new saod();
+Main s = new Main();
 s.Start();
 
-public class myList<T>
+internal class myList<T> : IEnumerable
 {
-    private const int _defaultCapacity = 4;
-    private T[] _items;
+    public T[] _items;
+    private int _lastIndex = -1;
     private int _size;
+    private const int _defaultCapacity = 4;
     static T[] _emptyArray = new T[0];
+
 
     public myList()
     {
@@ -28,6 +35,11 @@ public class myList<T>
             _items = _emptyArray;
         else
             _items = new T[capacity];
+    }
+
+    public myList(T[] list)
+    {
+        _items = list;
     }
 
     public int Count
@@ -56,29 +68,27 @@ public class myList<T>
         }
 
         if (index < _size)
-        {
             Array.Copy(_items, index, _items, index + 1, _size - index);
-        }
 
         _items[index] = item;
         _size++;
     }
 
-    public int FindIndex(Predicate<T> match)
+    public int FindIndex(Predicate<T> predicate)
     {
         Contract.Ensures(Contract.Result<int>() >= -1);
         Contract.Ensures(Contract.Result<int>() < Count);
-        return FindIndex(0, _size, match);
+        return FindIndex(0, _size, predicate);
     }
 
-    public int FindIndex(int startIndex, Predicate<T> match)
+    public int FindIndex(int startIndex, Predicate<T> predicate)
     {
         Contract.Ensures(Contract.Result<int>() >= -1);
         Contract.Ensures(Contract.Result<int>() < startIndex + Count);
-        return FindIndex(startIndex, _size - startIndex, match);
+        return FindIndex(startIndex, _size - startIndex, predicate);
     }
 
-    public int FindIndex(int startIndex, int count, Predicate<T> match)
+    public int FindIndex(int startIndex, int count, Predicate<T> predicate)
     {
         Contract.Ensures(Contract.Result<int>() >= -1);
         Contract.Ensures(Contract.Result<int>() < startIndex + count);
@@ -87,16 +97,16 @@ public class myList<T>
         int endIndex = startIndex + count;
         for (int i = startIndex; i < endIndex; i++)
         {
-            if (match(_items[i])) return i;
+            if (predicate(_items[i])) return i;
         }
         return -1;
     }
 
-    public T Find(Predicate<T> match)
+    public T Find(Predicate<T> predicate)
     {
         for (int i = 0; i < _size; i++)
         {
-            if (match(_items[i])) return _items[i];
+            if (predicate(_items[i])) return _items[i];
         }
 
         return default(T);
@@ -104,66 +114,38 @@ public class myList<T>
 
     public void ForEach(Action<T> action)
     {
-        for (int i = 0; i < _size; i++)
-        {
-            action(_items[i]);
-        }
+        foreach (var item in _items)
+            action(item);
     }
 
-    /*
-    public Enumerator GetEnumerator()
+    public IEnumerator GetEnumerator()
     {
-        return new Enumerator(this);
+        return _items.GetEnumerator();
     }
 
-    
-    public struct Enumerator
-    {
-        private List<T> list;
-        private int index;
-        private T current;
-
-        internal Enumerator(List<T> list)
-        {
-            this.list = list;
-            index = 0;
-            current = default(T);
-        }
-
-        public T Current
-        {
-            get
-            {
-                return current;
-            }
-        }
-    }
-    */
 }
 
-public class saod
+public class Main
 {
     public void Start()
     {
         myList<int> list = new myList<int>();
 
-        List<int> l = new List<int>();
+        for (int i = 1; i < 10; i++)
+            list.Add(i);
+        
+        foreach (int i in list)
+            Console.Write(i + " ");
 
-        for (int i = 0; i <= 10; i++)
-        {
-            list.Add((i));
-        }
+        Console.WriteLine();
 
-        Console.WriteLine(list.Count);
+        Console.WriteLine(list.FindIndex(i => 0 == i % 2) + " ");
 
-        list.Insert(0, 5);
 
-        Console.WriteLine(list.Count);
+        list.Insert(2, -1);
+        foreach (int i in list)
+            Console.Write(i + " ");
 
-        Console.WriteLine(list.FindIndex(item => item == 2));
-
-        Console.WriteLine(list.Find(item => item == 2));
-
-        //Console.WriteLine(list.ForEach());
+        Console.ReadLine();
     }
 }
